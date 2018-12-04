c635b4e8de83a234eb1b1974f86f67cd69a83f94
Nathan Wengert <nate.weng@gmail.com>
2018-12-03 22:24:47 -0700

    trying MX page

--- traversy_webscraping/scrape.js
+++ traversy_webscraping/scrape.js
@@ -3,14 +3,30 @@ const cheerio = require('cheerio')
 
 request('https://www.redolive.com/jobs/', (error, response, html) => {
     if(!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
+        const h = cheerio.load(html);
 
         console.log(`\nRED OLIVE JOB POSTINGS`)
-        const ROsectionHeading = $('h3');
+        const ROsectionHeading = h('h3');
         console.log(ROsectionHeading.text());
 
-        $('.title').each((e, el) => {
-            const item = $(el).text();
+        h('.title').each((e, el) => {
+            const item = h(el).text();
+            console.log('\t'+item);
+        });
+    }
+});
+
+
+request('https://data.mx.com/company#careers', (error, response, html) => {
+    if(!error && response.statusCode == 200) {
+        const i = cheerio.load(html);
+
+        console.log(`\nRED OLIVE JOB POSTINGS`)
+        const MXsection = i('h3');
+        console.log(MXsection.text());
+
+        i('.title').each((e, el) => {
+            const item = i(el).text();
             console.log('\t'+item);
         });
     }
@@ -22,14 +38,15 @@ request('https://company.bamboohr.com/jobs/', (error, response, html) => {
     if(!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
 
+
         console.log(`\nBambooHR JOB POSTINGS`)
         // const bambooHeading = $('.ResAts__card-content');
         // const bambooTitle = bambooHeading.find('h2').text();
         // console.log('?'+bambooTitle);
 
-        $('.ResAts__listing-link').each((e, el) => {
-            const bambooItem = $(el).text();
-            console.log('\t'+bambooItem);
-        });
+        // $('.ResAts__listing-link').each((e, el) => {
+        //     const bambooItem = $(el).text();
+        //     console.log('\t'+bambooItem);
+        // });
     }
 });
